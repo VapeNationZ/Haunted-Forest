e255df8524a4dd6d5737e53a482cb348e4e3628f
Joslyn Pereira <31210814+VapeNationZ@users.noreply.github.com>
2018-08-16 20:45:18 +0530

    Add files via upload
    Ball script
    here, ball=zombie

---
+++ Ball_script.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Ball_script : MonoBehaviour {
+
+
+    public Text CollisionText;
+
+
+
+	void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            CollisionText.text = "You Died!";
+
+        }
+    }
+}
+
