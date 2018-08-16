56ab45b9728ecce1ac407537544e2c7caa5d80a8
Joslyn Pereira <31210814+VapeNationZ@users.noreply.github.com>
2018-08-16 20:43:57 +0530

    Add files via upload
    Finish script

---
+++ fin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class fin : MonoBehaviour
+{
+
+
+    public Text CollisionText;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Ball")
+        {
+            GameObject.Find("Player").SendMessage("Finnish");
+
+
+        }
+    }
+}
