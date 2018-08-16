d7d64032b46d91cddb56d5432d5230cfe04d1753
Joslyn Pereira <31210814+VapeNationZ@users.noreply.github.com>
2018-08-16 20:43:09 +0530

    Add files via upload
    The end-game collision script

---
+++ collision_script.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class collision_script : MonoBehaviour {
+    
+
+    public Text CollisionText; 
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Ball")
+        {
+            CollisionText.text = "You Died!";
+            GameObject.Find("Player").SendMessage("Finnish");
+            
+        }
+    }
+}
